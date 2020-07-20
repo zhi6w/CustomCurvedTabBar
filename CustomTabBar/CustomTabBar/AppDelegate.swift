d7aae4bf5dee462904925167fdaf04887ed76696
Zane6w <zhwin@outlook.com>
2020-07-20 14:40:10 +0800

    fixed center button size

--- CustomTabBar/CustomTabBar/AppDelegate.swift
+++ CustomTabBar/CustomTabBar/AppDelegate.swift
@@ -13,9 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return [.all, .allButUpsideDown]
-    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
